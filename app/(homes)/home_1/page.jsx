c644a23903fcb139862dcd9b6fda7022fdaecbc8
adiapr <da801100@gmail.com>
2024-05-21 09:17:48 +0700

    Rev : Copyrught in home

--- app/(homes)/home_1/page.jsx
+++ app/(homes)/home_1/page.jsx
@@ -15,7 +15,7 @@ import SelectFilter from "@/components/hotels/filter-tabs/SelectFilter";
 
 export const metadata = {
   title: "Smartway Tours",
-  description: " Smartway adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group)."
+  description: "Smartway adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group)."
 };
 
 const Home_1 = () => {
