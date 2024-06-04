2acfbd28d0b9502d95feb8ae60b015ac4492d0c6
adiapr <da801100@gmail.com>
2024-06-04 18:50:39 +0700

    Change : title

--- app/(tour)/paket-wisata/[slug]/page.jsx
+++ app/(tour)/paket-wisata/[slug]/page.jsx
@@ -1,8 +1,8 @@
 import TourIndex from '@/components/tour-single/TourIndex';
 
 export const metadata = {
-  title: 'Tour Single | Smartway adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).',
-  description: 'GoTrip - Travel & Tour React NextJS Template',
+  title: 'Smartway.tours',
+  description: 'adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).',
 };
 
 
