0960115519a27efdb754fee11116c94a7af9f3da
adiapr <da801100@gmail.com>
2024-06-04 21:26:38 +0700

    Add ; Meta description

--- app/(tour)/paket-wisata/[slug]/page.jsx
+++ app/(tour)/paket-wisata/[slug]/page.jsx
@@ -1,19 +1,6 @@
-// import TourIndex from '@/components/tour-single/TourIndex';
-
-// export const metadata = {
-//   title: 'Smartway.tours',
-//   description: 'adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).',
-// };
-
-
-// const TourSingleV1Dynamic = async ({ params }) => {
-//   // console.log(params);
-//   return <TourIndex slug={params.slug} />;
-// };
-
-// export default TourSingleV1Dynamic;
 "use client";
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import TourIndex from '@/components/tour-single/TourIndex';
 
 // Fungsi untuk mengambil metadata dari API
@@ -21,11 +8,9 @@ const fetchTourData = async (slug) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/menu-wisata/${slug}`);
   const data = await response.json();
   return data;
-  
 };
 
-const TourSingleV1Dynamic = ({ params, data }) => {
-  
+const TourSingleV1Dynamic = ({ params }) => {
   const [title, setTitle] = useState('Smartway.tours');
   const [description, setDescription] = useState('adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).');
 
@@ -33,15 +18,8 @@ const TourSingleV1Dynamic = ({ params, data }) => {
     const updateMetadata = async () => {
       try {
         const data = await fetchTourData(params.slug);
-        console.log(data.name);
         setTitle(data.name);
         setDescription(data.description || 'adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).');
-        document.title = data.name + ' | Smartway Tours' || 'Smartway.tours'; // Update the page title
-        // Optionally, update the meta description tag
-        const metaDescription = document.querySelector('meta[name="description"]');
-        if (metaDescription) {
-          metaDescription.setAttribute('content', data.description || 'adalah travel agen profesional yang berdiri sejak 2016 dibawah naungan PT. Bagus Bahagia Bersama (BBB Group).');
-        }
       } catch (error) {
         console.error('Error fetching tour data:', error);
       }
@@ -50,8 +28,18 @@ const TourSingleV1Dynamic = ({ params, data }) => {
     updateMetadata();
   }, [params.slug]);
 
-  return <TourIndex slug={params.slug} />;
+  return (
+    <>
+      <Head>
+        <title>{title + ' | Smartway Tours'}</title>
+        <meta name="description" content={description} />
+        <meta name="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="https://smartway.tours/img/logo/logo-white.png"></meta>
+      </Head>
+      <TourIndex slug={params.slug} />
+    </>
+  );
 };
 
 export default TourSingleV1Dynamic;
-
