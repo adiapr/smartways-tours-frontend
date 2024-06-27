245ea8b7e7973753fe3452f53ff6fa058513b289
adiapr <da801100@gmail.com>
2024-06-27 18:13:47 +0700

    Blank page

---
+++ app/en/404/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlankEn() {
+  return (
+    <div>BlankEn</div>
+  )
+}
+
+export default BlankEn
---
+++ app/id/404/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlankId() {
+  return (
+    <div>BlankId</div>
+  )
+}
+
+export default BlankId
