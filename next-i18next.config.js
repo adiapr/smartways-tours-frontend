b69e24ab691dbb672f04d174fef38f696bb20ec7
adiapr <da801100@gmail.com>
2024-06-27 18:29:37 +0700

    rev 404

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'id', '404'],
+    locales: ['en', 'id', 'others'],
   },
   localePath: path.resolve('./public/locales'),
 };
